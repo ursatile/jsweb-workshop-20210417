a42fadfc2658de267f8b1525bc73718175eaddf4
Dylan Beattie <dylan@dylanbeattie.net>
2021-04-18 13:16:53 +0100

    Left/right moving in JSTris clone

--- jstris/js/game.js
+++ jstris/js/game.js
@@ -3,11 +3,23 @@ export class Position {
         this.rowIndex = rowIndex;
         this.colIndex = colIndex;
     }
+    move(direction) {
+        switch (direction) {
+            case 'left':
+                return new Position(this.rowIndex, this.colIndex - 1);
+            case 'right':
+                return new Position(this.rowIndex, this.colIndex + 1);
+        }
+    }
 }
 
 export class Cell {
     constructor(position) {
         this.position = position;
+    }
+
+    move(direction) {
+        this.position = this.position.move(direction);
     }
 
     get key() {
@@ -20,13 +32,20 @@ export class Block {
         this.name = name;
         this.cells = cells;
     }
+    move(direction) {
+        this.cells.forEach(cell => cell.move(direction));
+    }
 }
 
 export class Game {
+    move(direction) {
+        this.blocks.forEach(block => block.move(direction));
+    }
+
     addBlock() {
         let cells = [
-            new Cell(new Position(2,4)),
-            new Cell(new Position(3,4)),
+            new Cell(new Position(2, 4)),
+            new Cell(new Position(3, 4)),
         ];
         let block = new Block('orange-ricky', cells);
         this.blocks.push(block);
@@ -34,14 +53,14 @@ export class Game {
     constructor(rowCount, colCount) {
         this.rows = new Array();
         this.blocks = new Array();
-        for(let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+        for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
             let row = new Array();
-            for(let colIndex = 0; colIndex < colCount; colIndex++) {
+            for (let colIndex = 0; colIndex < colCount; colIndex++) {
                 let position = new Position(rowIndex, colIndex);
                 let cell = new Cell(position);
-                row.push(cell);                                
+                row.push(cell);
             }
-            this.rows.push(row);            
+            this.rows.push(row);
         }
     }
 
--- jstris/js/jstris.js
+++ jstris/js/jstris.js
@@ -4,16 +4,27 @@ import Renderer from './renderer.js';
 class JSTrisGameElement extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open'});
+        this.attachShadow({ mode: 'open' });
         this.renderer = new Renderer(this.shadowRoot);
     }
 
     connectedCallback() {
         let rowCount = parseInt(this.getAttribute("rows")) || 20;
         let colCount = parseInt(this.getAttribute("cols")) || 20;
-        let game = new Game(rowCount, colCount);
-        game.addBlock();
-        this.renderer.render(game);
+        this.game = new Game(rowCount, colCount);
+        this.game.addBlock();
+        this.renderer.render(this.game);
+        document.addEventListener("keydown", this.handleKeydown.bind(this));
+    }
+
+    handleKeydown(event) {
+        switch (event.code) {
+            case "ArrowLeft":
+                this.game.move('left'); break;
+            case "ArrowRight":
+                this.game.move('right'); break;
+        }
+        this.renderer.update(this.game);
     }
 }
 
