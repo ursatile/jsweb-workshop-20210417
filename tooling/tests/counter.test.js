8dda78386501f109a7e1ee6bbedc90e8fbe9a088
Dylan Beattie <dylan@dylanbeattie.net>
2021-04-18 12:31:43 +0100

    Added beforeEach() to interaction tests

--- tooling/tests/counter.test.js
+++ tooling/tests/counter.test.js
@@ -1,31 +1,27 @@
 import MyCounterElement from '../counter.js';
 
 describe('counting engine handles keyboard events', () => {
-    test('up arrow increments engine', () => {
-        let element = new MyCounterElement();
+    
+    let element;
+    let mockPreventDefault;
+    let fakeEvent;
+
+    beforeEach(() => {
+        mockPreventDefault = jest.fn();
+        element = new MyCounterElement();
         element.connectedCallback();
+        fakeEvent = { code: "ArrowUp", preventDefault: mockPreventDefault };
+    });
 
+    test('up arrow increments engine', () => {
         let mockIncrement = jest.fn();
-        let mockPreventDefault = jest.fn();
         element.engine.increment = mockIncrement;
-
-        let fakeEvent = { code: "ArrowUp", preventDefault: mockPreventDefault };
         element.handleKeydown(fakeEvent);
-
         expect(mockIncrement).toHaveBeenCalledTimes(1);
     });
 
     test('up arrow does not run default behaviour', () => {
-        let element = new MyCounterElement();
-        element.connectedCallback();
-
-        let mockIncrement = jest.fn();
-        let mockPreventDefault = jest.fn();
-        element.engine.increment = mockIncrement;
-
-        let fakeEvent = { code: "ArrowUp", preventDefault: mockPreventDefault };
         element.handleKeydown(fakeEvent);
-
         expect(mockPreventDefault).toHaveBeenCalledTimes(1);
     });
 
